3ab23503a9bcaa0a506d00cca49f27b6092d770d
Sven <chanck.mds@gmail.com>
2023-11-17 02:30:39 +0100

    Classes exercises are done

---
+++ Classes/exercise-2-create-a-child-class-start.ts
@@ -0,0 +1,68 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+class Currency {
+    private name: string;
+    private code: string;
+    private symbol: string;
+
+    constructor(name: string, code: string, symbol: string) {
+        this.name = name;
+        this.code = code;
+        this.symbol = symbol;
+    }
+
+    public describe(): void {
+        let description = `The ${this.name} currency `;
+        description += `has the code ${this.code} `;
+        description += `and uses the symbol ${this.symbol}.`;
+
+        console.log(description);
+    }
+
+    // Add a public method to get the name field.
+    public getName(): string {
+        return this.name;
+    }
+}
+
+// Alter this class to extend the `Currency` class.
+
+class DetailedCurrency extends Currency {
+
+    // Define a private field named `banknotes`.
+    // Set the field type so it can be used to store an array of numbers.
+    private banknotes: number[];
+
+    // Add a `banknotes` parameter with the same type as the `banknotes` field.
+    constructor(name: string, code: string, symbol: string, banknotes: number[]) {
+        // Fix this call to the parent constructor.
+        super(name, code, symbol);
+
+        // Store the value of the `banknotes` parameter in the corresponding field.
+        this.banknotes = banknotes;
+    }
+
+    // Change the visibility modifier on this method so it can be called
+    // by code outside of this class.
+    public describeBankNotes() {
+        // Access the parent class `name` field using the public method.
+        let description = `The ${this.getName()} currency uses the banknotes `;
+
+        // Replace `null` with the value of the `banknotes` field.
+        description += this.banknotes.join(', ');
+
+        console.log(description);
+    }
+}
+
+const euroCurrency = new DetailedCurrency("Euro", "EUR", "€", [500, 200, 100, 50, 20, 10, 5]);
+
+euroCurrency.describe();
+
+euroCurrency.describeBankNotes();
+
+// ----
+
+export {};
